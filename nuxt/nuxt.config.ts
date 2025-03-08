2e6cb20110f400d21579c95a84f08cd583147d5d
Juljus <juljus@gmail.com>
2025-03-08 23:30:51 +0200

    modules
    nuxtcontent and tailwindcss

--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['@nuxt/content', '@nuxtjs/tailwindcss'],
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true }
 })
