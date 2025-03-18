c1d2df36cd809a0abbcc1d5d323a777dba35c8f3
Juljus <juljus@gmail.com>
2025-03-18 09:28:23 +0200

    publish to gh-pages

--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: true,
   modules: ['@nuxt/content', '@nuxtjs/tailwindcss'],
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true }
