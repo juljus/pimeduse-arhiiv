eef37008a1ec13358e06ac0d75eeeb6b15a9db22
Juljus <juljus@gmail.com>
2025-03-18 13:49:25 +0200

    Update nuxt.config.ts

--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: "/pimeduse-arhiiv/", // 👈 Set this to match your GitHub repo name
+  },
   ssr: true,
   modules: ['@nuxt/content', '@nuxtjs/tailwindcss'],
   compatibilityDate: '2024-11-01',
