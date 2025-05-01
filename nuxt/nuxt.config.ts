c91430afdf79a5e2ee8f68318785f776cfc55dc0
Juljus <juljus@gmail.com>
2025-05-01 18:50:24 +0300

    Revert ".nojetkyll"
    This reverts commit 4027d4b7f7b88da817873326b6683b9e5ab29389.

--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
-    baseURL: "/pimeduse-arhiiv/", // as long as no custom domain is set
+    baseURL: "/pimeduse-arhiiv/", // 👈 Set this to match your GitHub repo name
   },
   ssr: true,
   modules: ['@nuxt/content', '@nuxtjs/tailwindcss'],
