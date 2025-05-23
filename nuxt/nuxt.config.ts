c3fd040be21a51fae6b0a74faea9b4953a94c07b
Juljus <juljus@gmail.com>
2025-05-23 21:28:15 +0200

    trigger webhook

--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -2,7 +2,6 @@
 export default defineNuxtConfig({
   modules: ['@nuxt/content', '@nuxtjs/tailwindcss', '@nuxt/image'],
   compatibilityDate: '2024-11-01',
-  
   devtools: { enabled: true },
   
   // Add site metadata with logo
