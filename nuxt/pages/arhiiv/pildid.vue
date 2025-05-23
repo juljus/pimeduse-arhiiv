4561d0946b4562da98739b3d204b3018e473d9e6
Juljus <juljus@gmail.com>
2025-05-23 19:56:54 +0200

    Revert "remove nuxt image"
    This reverts commit 4e83dae8f77406dfd4eb80d6d4498b4f4b8cdd0d.

--- nuxt/pages/arhiiv/pildid.vue
+++ nuxt/pages/arhiiv/pildid.vue
@@ -11,7 +11,7 @@
                 tabindex="0"
                 @keydown.enter="openPreview(image)"
                 :aria-label="`Image ${index + 1}: ${image.split('/').pop().replace(/\.[^/.]+$/, '')}`">
-                <img :src="image" :alt="`Pilt ${index + 1}`" class="w-full h-full object-cover rounded-md" loading="lazy" />
+                <NuxtImg :src="image" :alt="`Pilt ${index + 1}`" class="w-full h-full object-cover rounded-md" provider="ipx" sizes="sm:100vw md:50vw lg:200px" />
             </div>
         </div>
         
@@ -48,13 +48,17 @@
                             <div class="loader"></div>
                         </div>
                         
-                        <!-- Actual image -->
-                        <img 
+                        <!-- Actual image with very low quality placeholder to show immediately -->
+                        <NuxtImg 
                             :key="currentImage" 
                             :src="currentImage" 
                             class="modal-image" 
                             :alt="currentImageName || 'Preview'"
                             @click.stop
+                            :placeholder="[currentImage, { width: 20, quality: 5 }]" 
+                            provider="ipx"
+                            sizes="sm:100vw md:80vw lg:95vw"
+                            :modifiers="{quality: 85}"
                             fetchpriority="high"
                             loading="eager"
                             @load="onImageLoaded"
